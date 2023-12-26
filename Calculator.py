a87168e9f37779b67c9fcf91a853d07c3f5e4624
BXBigX <140758053+BXBigX@users.noreply.github.com>
2023-12-26 15:28:08 +0300

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -4,20 +4,20 @@ app = FastAPI()
 
 
 @app.get("/Sum/{a}/{b}")
-def calculated_plus(a: int, b: int):
+def calculated_plus(a: float, b: float):
     return {"Sum ==": a+b}
 
 
 @app.get("/Difference/{a}/{b}")
-def calculated_minus(a: int, b: int):
+def calculated_minus(a: float, b: float):
     return {"Difference ==": a-b}
 
 
 @app.get("/Division/{a}/{b}")
-def calculated_division(a: int, b: int):
+def calculated_division(a: float, b: float):
     return {"Division ==": a/b}
 
 
 @app.get("/Multiplication/{a}/{b}")
-def calculated_multiplication(a: int, b: int):
+def calculated_multiplication(a: float, b: float):
     return {"Multiplication ==": a*b}
